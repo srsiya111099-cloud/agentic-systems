4516d1473e76ec33cc81aad9ac986d288c4d7b09
srsiya111099-cloud <srsiya111099@gmail.com>
2026-05-12 23:19:06 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,11 @@
+
+numbers = [12, 7, 9, 20, 18, 5, 2, 11]
+
+print("Even numbers in the list:")
+for num in numbers:
+    if num % 2 == 0:
+        print(num)
+git add Module-1/Session-6-VCS/index.py
+git commit -m "Added index.py with even numbers logic"
+git push origin main
+
